c8ac225d93231ec1790a2008649afdb90d7b5a0d
haibuzou <haibuzouxi@163.com>
2016-08-18 19:40:38 +0800

    init commit

--- app/src/main/java/com/haibuzou/activitytask/LoginActivity.java
+++ app/src/main/java/com/haibuzou/activitytask/LoginActivity.java
@@ -1,20 +1,21 @@
 package com.haibuzou.activitytask;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
-public class FirstActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_second);
     }
 
     public void jump(View view){
-        Intent intent = new Intent(this,SecondActivity.class);
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/haibuzou/activitytask/SplashActivity.java
+++ app/src/main/java/com/haibuzou/activitytask/SplashActivity.java
@@ -1,21 +1,23 @@
 package com.haibuzou.activitytask;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class SecondActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_first);
+
     }
 
     public void jump(View view){
-        Intent intent = new Intent(this,MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent intent = new Intent(this,LoginActivity.class);
         startActivity(intent);
     }
 }
